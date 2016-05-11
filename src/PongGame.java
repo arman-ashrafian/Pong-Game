c3d5f2494c0fddb99ea88774f2cd49beae945f9a
Arman Ashrafian <ashy63@gmail.com>
2016-05-11 11:37:44 -0700

    Created bounds for the ball
    The ball now bounces when it reaches the paddle and edges of the screen.
    
    Added final variables for window height and width. Need to figure out
    how to further generalize the variables so it works on any screen size.

--- src/PongGame.java
+++ src/PongGame.java
@@ -10,56 +10,79 @@ import javax.swing.JFrame;
 import javax.swing.Timer;
 import javax.swing.WindowConstants;
 
-
 /**
  *
  * @author Arman Ashrafian
  */
-public class PongGame extends JComponent implements ActionListener, 
-                                                    MouseMotionListener {
+public class PongGame extends JComponent implements ActionListener,
+        MouseMotionListener {
+
+    private static final int WINDOW_WIDTH = 2000;
+    private static final int WINDOW_HEIGHT = 1800;
+    
     // Position of paddle
     private int paddleX = 0;
     private int paddleY = 700;
-    
+
     // Position of ball
     private int ballX = 1600;
     private int ballY = 800;
-    
+
+    private int ballYSpeed = -2;
+    private int ballXSpeed = -5;
+
     public static void main(String[] args) {
         JFrame window = new JFrame("Pong Game by Arman");
         PongGame game = new PongGame();
-        
+
         // Window Setup
         window.add(game);
         window.pack();
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         window.setLocationRelativeTo(null);
         window.setVisible(true);
-        window.setSize(2000,1800);
-        
+        window.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
         // Action Listener
-        Timer t = new Timer(100, game);
+        Timer t = new Timer(10, game);
         t.start();
         window.addMouseMotionListener(game);
     }
-    
+
     @Override
     protected void paintComponent(Graphics g) {
         //background
         g.setColor(Color.DARK_GRAY);
-        g.fillRect(0,0,2000,1800);
+        g.fillRect(0, 0, 4000, 4000);
         //paddle
         g.setColor(Color.WHITE);
-        g.fillRect(paddleX,paddleY,20,300);
+        g.fillRect(paddleX, paddleY, 20, 300);
         //draw ball
         g.setColor(Color.WHITE);
-        g.fillOval(ballX,ballY,75,75);
+        g.fillOval(ballX, ballY, 75, 75);
     }
 
     @Override
     public void actionPerformed(ActionEvent ae) {
-        ballX = ballX - 5;
-        ballY = ballY + 3;
+
+        if (ballX < paddleX + 20 && (ballY > paddleY && ballY < paddleY + 300)) {
+            ballXSpeed = 10;
+            ballYSpeed = 2;
+        }
+        if(ballY <= 0) {
+            ballYSpeed = 2;
+        }
+        if(ballX >= WINDOW_WIDTH - 75) {
+            ballXSpeed = -10;
+        }
+        if(ballY >= WINDOW_HEIGHT - 150) {
+            ballYSpeed = -4;
+        }
+        
+
+        ballX += ballXSpeed;
+        ballY += ballYSpeed;
+
         repaint();
     }
 
@@ -73,7 +96,4 @@ public class PongGame extends JComponent implements ActionListener,
     public void mouseDragged(MouseEvent me) {
     }
 
-    
-    
-    
 }
